066888f0c98317c9258421123938594240a02c29
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-10-24 08:53:38 -0500

    Added angular material, and transfer component w/ progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { FileTransferComponent } from './views/file-transfer/file-transfer.component';
+import { MatProgressBarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
-	{ path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'transfer', component: FileTransferComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FileTransferComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
 	HttpModule,
 	RouterModule.forRoot(appRoutes)
   ],
---
+++ src/app/views/file-transfer/file-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-transfer',
+  templateUrl: './file-transfer.component.html',
+  styleUrls: ['./file-transfer.component.scss']
+})
+export class FileTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
