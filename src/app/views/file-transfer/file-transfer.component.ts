d8b8f1a3b7bbd7c1a7099359f07884f76682cdd0
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-10-25 12:55:04 -0500

    Added progress bar label

--- src/app/views/file-transfer/file-transfer.component.ts
+++ src/app/views/file-transfer/file-transfer.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileTransferComponent implements OnInit {
 
+  currentSize: number = 1100000;
+  totalSize: number = 2200000000;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  abbreviateFileSize(oldSize: number, base: number) {
+    if(base < 1000) {
+      return `${oldSize} bytes`;
+    }
+    else if(base < 1000000) {
+      return `${oldSize/1000} kB`;
+    }
+    else if(base < 1000000000) {
+      return `${oldSize/1000000} MB`;
+    }
+    else {
+      return `${oldSize/1000000000} GB`;
+    }
+  }
+
+  getCurrentSize() {
+    return this.currentSize;
+  }
+
+  getTotalSize() {
+    return this.totalSize;
+  }
+
+  getTransferPercent() {
+    return this.getCurrentSize()/this.getTotalSize()*100;
   }
 
 }
