61c076e19fffe2c4db8bc154a28791538f725012
AJ DeTorrice <ajdetorrice@hotmail.com>
2017-11-09 14:27:55 -0600

    Added PeerService

---
+++ src/app/shared/api/peer.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PeerService {
+
+    file: File;
+	peer: any;
+	peerId: any;
+
+	constructor() {
+	// Replace '<key>' with actual key in prod, if there is anything other than '<key>' in this
+	// field please let me know Kellen
+	this.peer = new Peer({ key: '7599wxdge79442t9' });
+		setTimeout(() => {
+			this.peerId = this.peer.id;
+			console.log("PeerId:" + this.peer.id);
+		}, 3000);
+		this.peer.on('connection', function (conn) {
+			conn.on('data', function (data) {
+				console.log(data);
+			});
+		});
+	}
+	getPeerId(): any{
+		console.log("PeerId:" + this.peerId);
+		return this.peerId;
+	}
+	
+	// We only want this service to be able to initiate a connection with a remote peer
+	private connectToPeer(remotePeerId: any) {
+		var dataConn = this.peer.connect(remotePeerId);
+		console.log("Connecting to peer placeholder");
+	}
+	
+	// SQL server will have a table with two columns: {url}|{peerID}
+	createUrl(){
+		console.log("Creating url placeholder");
+		// Can be replaced with a more human-readable extension in the future
+		var urlext = Math.random().toString(36).substr(2,5);
+		console.log(this.peerId + ":" + urlext);
+		return urlext;
+	}
+
+}
