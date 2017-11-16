b3cc70c195af1b57bd2be79020418b4a511519f6
AJ DeTorrice <ajdetorrice@hotmail.com>
2017-11-16 08:48:08 -0600

    Refactored queries

--- server/db/db.js
+++ server/db/db.js
@@ -1,28 +1,32 @@
 const { Client } = require('pg');
 const client = new Client({
-		user: '',
-		host: '',
-		database: '',
-		password: '',
+		user: 'P2pft',
+		host: 'localhost',
+		database: 'P2PFileTransfer',
+		password: 'curTestPass$115',
 		port: 3211,
 });
 exports.connect = function(done) {
 	client.connect();
 }
 exports.getByUrl = function(url, done){
-	const query = 'SELECT peerId FROM <tablename> WHERE url = ?';
-	client.query(query, url)
-		.then(res => {
-			console.log("GetbyUrl query completed successfully");
-		})		
-		.catch(e => console.error(e.stack))
+	const query = 'SELECT peerid FROM host_data WHERE url = ?';
+	client.query(query, url,(err, res) => {
+		if(err){
+			console.log("Error: " + err.stack);
+		} else {
+			console.log(res.rows[0]);
+		}
+	})
 }
 
 exports.create = function(peerId, url, done){
-	const query = 'INSERT INTO <tablename> SET peerId=$1 url=$2';
-	client.query(query, [peerId, url])
-		.then(res =>{
-			console.log("Create query completed successfully");
-		})
-		.catch(e => console.error(e.stack))
+	const query = 'INSERT INTO host_data SET peerid=$1 url=$2';
+	client.query(query, [peerId, url],(err, res) => {
+		if(err){
+			console.log("Error: " + err.stack);
+		} else {
+			console.log(res.rows[0]);
+		}
+	})
 }
