5249a9de645ff5e76d8c5ce80771c88bccc3c5f0
AJ DeTorrice <ajdetorrice@hotmail.com>
2017-11-26 22:54:26 -0600

    Added ApiService to talk to DB

---
+++ src/app/shared/api/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+// import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ApiService {
+
+    _apiUrl = '/api';
+
+    constructor(private http: HttpClient){}
+
+    getPeerByUrl(url: string): Observable<any> {
+		let res = this.http.get(this._apiUrl + '/' + url);
+		console.log(res);
+        return res;
+    }
+
+    addPeer(peer: string, url: string): Observable<any> {
+        console.log("addPeer method: " + peer + ":" + url);
+        return this.http.post(this._apiUrl, 
+        {
+            "peerid": peer,
+            "url": url
+        })
+    }
+}
